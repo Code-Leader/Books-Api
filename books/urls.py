9fcaff19b733dad5f35dd2989fb4e443477e1e64
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-05 21:19:47 +0500

    Code

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BookListApiView
+from .views import BookListApiView, book_list_view
 
 urlpatterns = [
     path('', BookListApiView.as_view()),
+    path('books/', book_list_view)
 ]
--- books/views.py
+++ books/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import generics
 
 
 class BookListApiView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
+# function based view in DRF
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+    book = Book.objects.all()
+    serializer = BookSerializer(book, many=True)
+    return Response(serializer.data)
