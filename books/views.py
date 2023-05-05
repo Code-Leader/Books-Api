594f6af5bbb0c4f843e838d32b1154a06c9eb6e9
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-06 00:39:19 +0500

    This is the api view code

--- books/views.py
+++ books/views.py
@@ -2,38 +2,102 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
 
 
-class BookListApiView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# Bunda biz obyektalni ro'yhat ko'rinshda chiqramiz
+# class BookListApiView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
-class BookDetailApiView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookListApiView(APIView):
 
+    def get(self,request):
+        books = Book.objects.all()
+        serializer_deta = BookSerializer(books, many=True).data
+        data = {
+            'status': f"Returned {len(books)} books",
+            'books': serializer_deta
+
+        }
+        return Response(data)
+
+
+# bunda biz obyktlarni malumtini olamiz
+# class BookDetailApiView(generics.RetrieveAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookDetailApiView(APIView):
+
+    def get(self,request, pk):
+        try:
+
+            book = Book.objects.get(id=pk)
+            serializer_data = BookSerializer(book).data
+
+            data = {
+                "status": "Successfull",
+                "book": serializer_data
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception:
+            return Response(
+                {"status": "False",
+                 "message": "Book is not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+
+
+# bunda biz obyektni ochirib yuborishimiz mumkun
 class BookDeleteApiView(generics.DestroyAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+# bunda biz obyektni ozgaritishimiz mumkun yani edit qilshimiz mumkun
 class BookUpdateApiView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookCreateApiView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    
+
+# bunda biz obiyekt yartishimiz mumkun
+# class BookCreateApiView(generics.CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookCreateApiView(APIView):
+
+    def post(self, request):
+        data = request.data
+        serailazer = BookSerializer(data=data)
+        if serailazer.is_valid(raise_exception=True):
+            books = serailazer.save()
+
+            data = {'satus': "Books are saved to the detabase",
+                    'books': data
+                    }
+            
+            return Response(data)
+
+
+
+
+# bunda biz obeyktin korishimiz hamda yarishimiz mumkun
 class BookListCreateApiView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+# bunda biz obyektni edit qishimz hamda ochirishimiz mumkun
 class BookUpdateDeleteApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
 # function based view in DRF
 @api_view(['GET'])
