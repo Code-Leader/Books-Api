89f792b3a54165066cea023cb51c2d635beca954
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-05 23:06:55 +0500

    swagger update and add redoc

--- library_project/urls.py
+++ library_project/urls.py
@@ -6,26 +6,26 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Books API",
+      title="Books list API",
       default_version='v1',
       description='Library demo project',
       terms_of_service='demo.com',
-      contact=openapi.Contact(email='iviuzbek@gmail.com')
-      license=openapi.license=(name="demo licence")
-   )
+      contact=openapi.Contact(email='iviuzbek@gmail.com'),
+      license=openapi.License(name="demo licence"),
+    ),
    public=True,
-   permissions_classes=(permissions.AllowAny, )
+   permission_classes=[permissions.AllowAny],
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('books.urls')),
 
     # swagger
     path('swagger/', schema_view.with_ui(
-        'swagger', cache_timeout=0, name='swagger-swagger-ui'),
-        ('redoc/', schema_view.with_ui(
-            'redoc', cache_timeout=0, name='schema-redoc'),
-        )
-        )
+        'swagger', cache_timeout=0), name='swagger-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
+    
         
 ]
