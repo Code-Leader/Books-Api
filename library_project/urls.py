74911fe1bdd6eca55fefbddff0714771c34524c9
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-05 22:48:35 +0500

    swagger

--- library_project/urls.py
+++ library_project/urls.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Books API",
+      default_version='v1',
+      description='Library demo project',
+      terms_of_service='demo.com',
+      contact=openapi.Contact(email='iviuzbek@gmail.com')
+      license=openapi.license=(name="demo licence")
+   )
+   public=True,
+   permissions_classes=(permissions.AllowAny, )
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('books.urls')),
+
+    # swagger
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0, name='swagger-swagger-ui'),
+        ('redoc/', schema_view.with_ui(
+            'redoc', cache_timeout=0, name='schema-redoc'),
+        )
+        )
+        
 ]
