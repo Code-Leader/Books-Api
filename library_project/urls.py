5322512d0cfaf39542b1983d467139ac5889af00
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-06 20:18:10 +0500

    rest login logout

--- library_project/urls.py
+++ library_project/urls.py
@@ -20,8 +20,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('books.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
 
     # swagger
     path('swagger/', schema_view.with_ui(
